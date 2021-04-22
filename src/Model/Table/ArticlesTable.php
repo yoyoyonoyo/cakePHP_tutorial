199c816d48f4ec49bc9cd40397d1c7e0929c27f5
益山 曜 <y-masuyama@b-tm.co.jp>
2021-04-22 11:27:04 +0900

    add tag

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -19,13 +19,46 @@ class ArticlesTable extends Table
     }
 
     public function beforeSave($event, $entity, $options){
+        if ($entity->tag_string) {
+            $entity->tags = $this->_buildTags($entity->tag_string);
+        }
         if ($entity->isNew() && !$entity->slug) {
             $sluggedTitle = Text::slug($entity->title);
             // スラグをスキーマで定義されている最大長に調整
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
     }
+    protected function _buildTags($tagString)
+    {
+        // タグをトリミング
+        $newTags = array_map('trim', explode(',', $tagString));
+        // 全てのからのタグを削除
+        $newTags = array_filter($newTags);
+        // 重複するタグの削減
+        $newTags = array_unique($newTags);
 
+        $out = [];
+        $query = $this->Tags->find()
+            ->where(['Tags.title IN' => $newTags]);
+
+        // 新しいタグのリストから既存のタグを削除。
+        foreach ($query->extract('title') as $existing) {
+            $index = array_search($existing, $newTags);
+            if ($index !== false) {
+                unset($newTags[$index]);
+            }
+        }
+        // 既存のタグを追加。
+        foreach ($query as $tag) {
+            $out[] = $tag;
+        }
+        // 新しいタグを追加。
+        foreach ($newTags as $tag) {
+            $out[] = $this->Tags->newEntity(['title' => $tag]);
+        }
+        return $out;
+    }
+    
     public function validationDefault(Validator $validator)
     {
         $validator
